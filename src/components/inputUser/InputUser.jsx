1e5244211a1230236bba1559286f7da0d1f880e1
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-31 03:08:57 +0900

    Fix: 에러 컨트롤

--- src/components/inputUser/InputUser.jsx
+++ src/components/inputUser/InputUser.jsx
@@ -2,28 +2,12 @@ import React, { forwardRef } from 'react';
 import styled from 'styled-components';
 import { Button } from '../../components';
 
-const InputUserContainer = styled.div`
-  width: 100%;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  gap: 10px;
-
-  input {
-    width: 240px;
-    border: none;
-  }
-  button {
-    width: 80px;
-  }
-`;
-
 function InputUser({ onCreate }, ref) {
   return (
-    <InputUserContainer>
+    <div>
       <input ref={ref} placeholder="구성원을 입력하세요" required />
       <Button onClick={onCreate}>등록</Button>
-    </InputUserContainer>
+    </div>
   );
 }
 
