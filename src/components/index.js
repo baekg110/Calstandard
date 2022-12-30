909492002cdeaa3b2859a02c0563d90e7b036a65
baek gyeonghyeon <baekg.dev@gmail.com>
2022-12-31 03:26:53 +0900

    Design: 구성원 입력창 스타일링

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,3 @@ export { default as PaymentList } from './paymentList/PaymentList';
 export { default as ResultList } from './resultList/ResultList';
 export { default as PayCheck } from './payCheck/PayCheck';
 export { default as DetailModal } from './detailModal/DetailModal';
-
-export { default as Button } from './button/Button';
--- src/components/inputUser/InputUser.jsx
+++ src/components/inputUser/InputUser.jsx
@@ -1,11 +1,55 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
+import styled, { css } from 'styled-components';
+const InputContainer = styled.div`
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  gap: 16px;
 
+  input {
+    padding: 6px 12px;
+    width: 240px;
+    border-radius: 6px;
+    border: 1px solid var(--border-color);
+  }
+
+  button {
+    width: 60px;
+    background-color: var(--main-color);
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+
+    &:disabled{
+    background-color: #ddd;
+
+    }
+`;
+
+const MAX_LENGTH = 18;
 function InputUser({ onCreate }, ref) {
+  const [activ, setActiv] = useState(true);
+  const handleActiv = () => {
+    if (ref.current.value) {
+      setActiv(false);
+    } else {
+      setActiv(true);
+    }
+  };
+
+  // const onBlur = () => {
+  //   if (ref.current.value.length > MAX_LENGTH) {
+  //     ref.current.value = ref.current.value.slice(0, MAX_LENGTH);
+  //   }
+  // };
   return (
-    <div>
-      <input ref={ref} placeholder="계정명" required />
-      <button onClick={onCreate}>등록</button>
-    </div>
+    <InputContainer>
+      <input maxLength={MAX_LENGTH} ref={ref} onChange={handleActiv} placeholder="구성원 이름을 입력해주세요." />
+      <button onClick={onCreate} disabled={activ}>
+        등록
+      </button>
+    </InputContainer>
   );
 }
 
